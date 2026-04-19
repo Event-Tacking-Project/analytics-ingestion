53f5b5e8aeb91f1ab88e21d95e2695390147c598
SREEMT <tmeers101@gmail.com>
2026-04-19 13:52:53 -0400

    Implemented event routes #1 - need testing - need to finish logic

--- src/analytics-ingestion.cr
+++ src/analytics-ingestion.cr
@@ -1,13 +1,10 @@
 require "kemal"
+require "./routes/track.routes.cr"
 
 # TODO: Write documentation for `Analytics::Ingestion`
 module Analytics::Ingestion
   VERSION = "0.1.0"
 
   # TODO: Put your code here
-  get "/" do
-    "Hello World!"
-  end
-
   Kemal.run
 end
